b5711e8b0303ee5e562819d9312a505d2c686fa8
darshanraju <darshan.raju99@gmail.com>
2021-07-17 20:15:31 +1000

    Use random number or counter for tokenID's

--- contracts/StudentNFTContractV2.sol
+++ contracts/StudentNFTContractV2.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 /**
-This contract is passed a random number which is used as tokenId's for minting
-This random number might come from a ChainlinkVRF call
+This contract uses a counter stored inside the contract as tokenId for minting
  */
 
 contract StudentNFTContract is ERC721 {
 
     mapping (uint256 => string) private _tokenURIs;
+    uint256 tokenID = 0;
 
     constructor(string memory _name, string memory _symbol)
         ERC721(_name, _symbol)
@@ -27,17 +27,16 @@ contract StudentNFTContract is ERC721 {
         : "";
     }
 
-    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
-        require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
-        _tokenURIs[tokenId] = _tokenURI;
+    function _setTokenURI(uint256 _tokenId, string memory _tokenURI) internal virtual {
+        _tokenURIs[_tokenId] = _tokenURI;
     }
 
     function mint(
         address _to,
-        uint256 _tokenId,
         string memory tokenURI_
     ) external {
-        _mint(_to, _tokenId);
-        _setTokenURI(_tokenId, tokenURI_);
+        tokenID += 1;
+        _mint(_to, tokenID);
+        _setTokenURI(tokenID, tokenURI_);
     }
 }
