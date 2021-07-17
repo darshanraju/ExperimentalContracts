59ed959b4d22d2ddd185f4447b9051924ae6c783
darshanraju <darshan.raju99@gmail.com>
2021-07-17 19:50:14 +1000

    cleaning code

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,11 +10,16 @@ module.exports = {
     localhost: {
       url: "http://127.0.0.1:8545",
     },
-    // kovan: {
-    //   url: process.env.INFURA_KOVAN_KEY,
-    //   accounts: [process.env.ACCOUNT],
-    //   saveDeployments: true,
-    // },
+    kovan: {
+      url: "https://kovan.infura.io/v3/25ef01a5beaa424785dd92820a5a8cb5",
+      accounts: [process.env.KOVAN_ACCOUNT],
+      saveDeployments: true,
+    },
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/25ef01a5beaa424785dd92820a5a8cb5",
+      accounts: [process.env.RINKEBY_ACCOUNT],
+      saveDeployments: true,
+    },
   },
   solidity: {
     version: "0.8.0",
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,43 +1,5 @@
-const MockOpenSeaMetaData = {
-  description: "a bunch of description text",
-  external_url: "https://openseacreatures.io/3",
-  image:
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmemestatic.fjcdn.com%2Fpictures%2FRunescape_8449f5_6366030.jpg",
-  name: "Darshan Darshan",
-  attributes: [
-    {
-      trait_type: "Base",
-      value: "Starfish",
-    },
-    {
-      trait_type: "Level",
-      value: 5,
-    },
-  ],
-};
-
 async function main() {
-  //Get connection to etherium using ethers lib
   const [deployer] = await ethers.getSigners();
-
-  console.log("Depoying contracts with the account: ", deployer.address);
-
-  console.log("Account balance: ", (await deployer.getBalance()).toString());
-  console.log("---------------------1----------------------");
-
-  //Getting compiled smart contract
-  console.log("Basic Contract Deployment");
-  const MyFirstContract = await ethers.getContractFactory("Contract");
-  const deployedContract = await MyFirstContract.deploy();
-
-  console.log("Deployed  Storage contract address: ", deployedContract.address);
-
-  await deployedContract.setNumber(7);
-
-  let result = BigInt(await deployedContract.getNumber()).toString();
-  console.log("Stored value in contract is: ", result);
-
-  console.log("---------------------2----------------------");
 
   const NFT_CONTRACT_NAME = "niftyVerse";
   const NFT_CONTRACT_SYMBOL = "NV";
@@ -49,27 +11,15 @@ async function main() {
   );
 
   console.log("Deployed NFT contract address: ", deployedNFTContract.address);
+  const tokenURI =
+    "https://ipfs.io/ipfs/QmbBnUjyHHN7Ytq9xDsYF9sucZdDJLRkWz7vnZfrjMXMxs?filename=2-ST_BERNARD.json";
   let tokenID = 1;
 
-  console.log("FIRST MINT");
-  const tokenURI =
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmemestatic.fjcdn.com%2Fpictures%2FRunescape_8449f5_6366030.jpg&f=1&nofb=1";
+  console.log("Minting first NFT...");
+
   await deployedNFTContract.mint(deployer.address, tokenID, tokenURI);
   const NFTURI1 = await deployedNFTContract.tokenURI(tokenID);
   console.log(`First NFT MetaData: ${NFTURI1}`);
-
-  console.log("SECOND MINT");
-  tokenID += 1;
-  await deployedNFTContract.mint(
-    deployer.address,
-    tokenID,
-    JSON.stringify(MockOpenSeaMetaData)
-  );
-
-  const NFTURI2 = await deployedNFTContract.tokenURI(tokenID);
-
-  console.log("Second NFT MetaData: ");
-  console.log(JSON.parse(NFTURI2));
 }
 
 main();
